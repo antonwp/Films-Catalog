b37780a8a5288112ba8bf7a27bf0b5de6b0fce9d
Anton <code-name-5@yandex.ru>
2021-07-19 18:03:52 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      }
+    },
+  },
+  variants: {
+    extend: {
+      animation: []
+    },
+  },
+  plugins: [
+    require('tailwindcss-animatecss')({
+      classes: ['animate__animated', 'animate__fadeIn', 'animate__bounceIn', 'animate__lightSpeedOut'],
+      settings: {
+        animatedSpeed: 1000,
+        heartBeatSpeed: 1000,
+        hingeSpeed: 2000,
+        bounceInSpeed: 750,
+        bounceOutSpeed: 750,
+        animationDelaySpeed: 1000
+      },
+      variants: ['responsive', 'hover', 'reduced-motion'],
+    }),
+    require('tailwind-scrollbar-hide')
+  ],
+}
